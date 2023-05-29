4746dc5a0877a32ae3e7d43e570051169d0c905b
redfl0wer <125115138+redfl0wer@users.noreply.github.com>
2023-05-29 12:21:17 +0900

    add KBO

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const MLBButton = document.getElementById('league-select--MLB');
 const NPBButton = document.getElementById('league-select--NPB');
+const KBOButton = document.getElementById('league-select--KBO');
 const MLBList = document.querySelector('.ballpark-list--MLB');
 const NPBList = document.querySelector('.ballpark-list--NPB');
+const KBOList = document.querySelector('.ballpark-list--KBO');
 
 MLBButton.addEventListener('click', function() {
   MLBList.style.display = 'block';
   NPBList.style.display = 'none';
+  KBOList.style.display = 'none';
 });
 
 NPBButton.addEventListener('click', function() {
   MLBList.style.display = 'none';
   NPBList.style.display = 'block';
+  KBOList.style.display = 'none';
+});
+
+KBOButton.addEventListener('click', function() {
+  MLBList.style.display = 'none';
+  NPBList.style.display = 'none';
+  KBOList.style.display = 'block';
 });
